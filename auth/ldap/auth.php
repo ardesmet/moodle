72ea63fa28515138368f8bfbdbfb1d099efae57a
De Smet <arnaud.desmet@uclouvain-mons.be>
2013-05-24 10:08:57 +0200

    [sc25.1] SSO Chrome

--- auth/ldap/auth.php
+++ auth/ldap/auth.php
@@ -1580,7 +1580,7 @@ class auth_plugin_ldap extends auth_plugin_base {
             // Now start the whole NTLM machinery.
             if(!empty($this->config->ntlmsso_ie_fastpath)) {
                 // Shortcut for IE browsers: skip the attempt page
-                if(check_browser_version('MSIE')) {
+                if(check_browser_version('MSIE') || (check_browser_version('Chrome'))) {
                     $sesskey = sesskey();
                     redirect($CFG->wwwroot.'/auth/ldap/ntlmsso_magic.php?sesskey='.$sesskey);
                 } else {
