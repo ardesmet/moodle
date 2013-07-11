b0afa9de0ae49bda90da12b175c437947c44d2a0
De Smet <arnaud.desmet@uclouvain-mons.be>
2013-07-11 13:36:06 +0200

    [sc25.11]Ne pas afficher les cours cacher sur la page d accueil

--- admin/cli/renamer.php
+++ admin/cli/renamer.php
@@ -2,15 +2,22 @@
 
 require(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once($CFG->dirroot.'/course/lib.php');
+require_once($CFG->libdir. '/coursecatlib.php');
 global $DB;
-//$courses = get_courses(2);
-// foreach ($courses as $course) {
-////     print("Modification du cours $course->fullname $course->shortname...");
-////     $course->fullname = '[2012-2013] '.$course->fullname;
-////     $course->shortname = '[2012-2013] '.$course->shortname;
-////     if($DB->update_record('course', $course))
-////        print("Cours modifié!\n");
-////     else die();
+$courses = get_courses(7);
+foreach ($courses as $course) {
+     print("Modification du cours $course->fullname $course->shortname...");
+     $course->fullname = '[2012-2013]'.$course->fullname;
+     $course->shortname = '[2012-2013]'.$course->shortname;
+     $course->idnumber = '[2012-2013]'.$course->idnumber;
+     if($DB->update_record('course', $course))
+        print("Cours modifié!<br>");
+     
+///     else die();
 //     
-//	}
-echo coursecat::get(2)->delete_move(5);
+	}
+
+if(coursecat::get(7)->delete_move(9)) echo 'cours déplacés dans les archives';
+else echo 'problème...';
+//echo 'a';
+//print_object(coursecat::get(2)->can_move_content_to(5));
